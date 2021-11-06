40224b3c4b1ec03cf4841006020b2226097a8e84
alexurbanskyi <alexurbanvin@gmail.com>
2021-11-06 14:31:54 +0200

    footer hireme

---
+++ src/js/main.js
@@ -0,0 +1,24 @@
+
+// $(document).ready(() => {
+//    $('.footer__btn').on('click', function () {
+     
+//        if($('.hireme').hasClass('active')) {
+//            $('.hireme').removeClass('active');
+//        } else {
+//            $('.hireme').addClass('active');
+//        }
+//    })
+// });
+
+
+// $(document).ready(() => {
+//    $('.hireme__close').on('click', function () {
+     
+//        if($('.hireme').hasClass('active')) {
+//            $('.hireme').removeClass('active');
+//        } else {
+//            $('.hireme').addClass('active');
+//        }
+//    })
+// });
+
